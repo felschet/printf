7c8094417c0a128bbd3373332b40f868dbbc6610
felschet <felsto13@stud.thga.de>
2023-05-07 14:21:38 +0200

    Farbige Eule hinzufügen

---
+++ ansi_color.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf("\033[1;31m"); // setze Farbe auf rot
+	printf(".___.\n");
+	printf("{o,o}\n");
+	printf("/)__)\n"):
+	printf("_"_"_\n");
+	printf("\033[0m"); // setze Farbe zurück auf Standardfarbe
+	return 0;
+}
+
